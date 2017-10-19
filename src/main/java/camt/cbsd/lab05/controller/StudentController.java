59990063fe327f50ea80d0c2e50b45caf5aba0d8
nuBoreZ <kritsanah_supphasri@cmu.ac.th>
2017-10-19 23:04:43 +0700

    Stuck at 2.3

--- src/main/java/camt/cbsd/lab05/controller/StudentController.java
+++ src/main/java/camt/cbsd/lab05/controller/StudentController.java
@@ -3,16 +3,25 @@ package camt.cbsd.lab05.controller;
 import camt.cbsd.lab05.entity.Student;
 import camt.cbsd.lab05.service.StudentService;
 import camt.cbsd.lab05.service.StudentServiceImpl;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
 import javax.websocket.server.PathParam;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Paths;
 import java.util.List;
 
 @RestController
 public class StudentController {
+    String imageServerDir;
     StudentService studentService;
     @Autowired
     public void setStudentService(StudentService studentService) {
@@ -41,5 +50,16 @@ public class StudentController {
         System.out.println(student);
         return ResponseEntity.ok(student);
     }
+    @GetMapping(
+            value = "/images/{fileName:.+}",
+            produces = {MediaType.IMAGE_GIF_VALUE,
+            MediaType.IMAGE_JPEG_VALUE,MediaType.IMAGE_PNG_VALUE})
+    public @ResponseBody
+    ResponseEntity<?> getStudentImage(@PathVariable("fileName")String fileName)throws IOException{
+        File file = Paths.get(imageServerDir+fileName).toFile();
+        InputStream in = new FileInputStream(file);
+        return ResponseEntity.ok(IOUtils.toByteArray(in));
+    }
+
 
 }
